0352ac1c500dd0cbe494b6ed571e4df8b4af3fa9
FREDY ANTONIO MAZO QUICENO <frema1914@gmail.com>
2023-10-04 10:03:05 -0500

    Project created in ASP.NET Core MVC

---
+++ ShoppingStore_DlloSat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoppingStore_DlloSat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
