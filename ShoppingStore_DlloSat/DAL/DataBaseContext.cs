09fe4d1f8e57bddb9b4c87a3260e8601051dee02
FREDY ANTONIO MAZO QUICENO <frema1914@gmail.com>
2023-10-04 22:55:04 -0500

    Last Change

---
+++ ShoppingStore_DlloSat/DAL/DataBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingStore_DlloSat.DAL.Entities;
+using System.Data.Common;
+
+namespace ShoppingStore_DlloSat.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        //Constructor
+        public DataBaseContext(DbContextOptions<DataBaseContext>options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Country> Countries { get; set; }
+     }
+}
---
+++ ShoppingStore_DlloSat/DAL/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingStore_DlloSat.DAL.Entities
+{
+    //Primera entidad que se convertirá en tabla de la BD
+    public class Country : Entity //Hereda de Entity
+    {
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]//Controlar el máximo de caracteres
+
+        [Required(ErrorMessage = "¡El campo {0} es requerido! ")]
+        public string Name { get; set; }
+    }
+}
